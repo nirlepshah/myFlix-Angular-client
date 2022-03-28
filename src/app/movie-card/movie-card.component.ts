da1cbd47cf03bdc5d935550c63ef8704b99bc66f
nirlepshah <nirlepshah@gmail.com>
2022-03-27 23:30:16 -0400

    Add addToUserFavs() and  DeleteFavs() functions

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
 
 
 @Component({
@@ -8,13 +14,20 @@ import { FetchApiDataService } from '../fetch-api-data.service'
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent {
+  user: any = {};
+  Username = localStorage.getItem('user');
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
-
+  currentUser: any = null;
+  currentFavs: any = null;
+  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog, public snackBar: MatSnackBar, public router: Router) { }
+  
 ngOnInit(): void {
   this.getMovies();
+  this.getCurrentUser();
 }
 
+
+//Get all movies
 getMovies(): void {
   this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
@@ -22,4 +35,63 @@ getMovies(): void {
       return this.movies;
     });
   }
+  // open Director dialog
+  openDirectorDialog(name: string, bio: string, birth: string): void {
+    this.dialog.open(DirectorCardComponent, {
+      data: {Name: name, Bio: bio, Birth: birth},
+      width: '500px',
+    });
+  }
+  // Open Genre View
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreCardComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '500px'
+    });
+  }
+  openSynopsis(title: string, imagePath: any, description: string): void {
+    this.dialog.open(SynopsisCardComponent, {
+      data: {
+        Title: title,
+        ImagePath: imagePath,
+        Description: description,
+      },
+      width: '500px'
+    });
+   
+  }
+  getCurrentUser(): void {
+    const username = localStorage.getItem('user');
+    this.fetchApiData.getUser(username).subscribe((resp: any) => {
+       
+     console.log(resp)
+      const currentUser=resp.Username
+      console.log(currentUser)
+      const currentFavs=resp.FavouriteMovies
+      console.log(currentFavs)
+
+    });
+  }
+  addToUserFavs(id: string): void {
+    console.log(id);
+    const token = localStorage.getItem('token');
+    console.log(token)
+    this.fetchApiData.addFavMovie(id).subscribe((response: any) => {
+      
+      console.log(response);
+    });
+    
+    }
+    DeleteFavs(id: string): void {
+      console.log(id);
+        this.fetchApiData.deleteFavMovie(id).subscribe((response: any) => {
+        console.log(response);
+      });
+      
+      }
+   
+
 }
