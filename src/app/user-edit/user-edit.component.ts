a7e19835c40f9df3c65c14f1cb057c93cdb04068
nirlepshah <nirlepshah@gmail.com>
2022-03-27 23:31:13 -0400

    Create user-edit.component.ts

---
+++ src/app/user-edit/user-edit.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.scss']
+})
+export class UserEditComponent implements OnInit {
+  Username = localStorage.getItem('user');
+  user: any = {};
+
+  @Input() userData = { 
+    Username: this.user.Username,
+    Password: this.user.Password,
+    Email: this.user.Email,
+    Birthday: this.user.Birthday
+  }
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserEditComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser(user).subscribe((resp: any) => {
+      this.user = resp;
+      console.log(this.user);
+      return this.user
+    });
+  }
+
+  editUserProfile(): void {
+    this.fetchApiData.editUserProfile(this.userData).subscribe((resp) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', resp.Username);
+      this.snackBar.open('Your profile was updated successfully.', 'OK', {
+        duration: 2000
+      });
+      setTimeout(() => {
+        window.location.reload();
+      });
+    });
+  }
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
