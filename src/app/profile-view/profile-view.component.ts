bdeb64cc8fb07677b1995d2e04c424a99b55d174
nirlepshah <nirlepshah@gmail.com>
2022-03-27 23:30:46 -0400

    Create profile-view.component.ts

---
+++ src/app/profile-view/profile-view.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UserEditComponent } from '../user-edit/user-edit.component';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
+
+
+@Component({
+  selector: 'app-profile-view',
+  templateUrl: './profile-view.component.html',
+  styleUrls: ['./profile-view.component.scss']
+})
+export class ProfileViewComponent implements OnInit {
+  user: any = {};
+  movies: any[] = [];
+  userName: any = localStorage.getItem('user');
+  
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+    
+  }
+ 
+  getUser(): void {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.fetchApiData.getUser(user).subscribe((resp: any) => {
+        this.user = resp;
+        console.log(this.user);
+      });
+    }
+  }
+  
+  openEditUserProfile(): void {
+    this.dialog.open(UserEditComponent, {
+      width: '500px'
+    });
+  }
+  
+  // Delete User account
+ 
+  deleteUserProfile(): void {
+    if (confirm('Are you sure? This cannot be undone.')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('Your account was deleted', 'OK', {duration: 6000});
+      });
+      this.router.navigate(['welcome'])
+      this.fetchApiData.deleteUserProfile().subscribe(() => {
+        localStorage.clear();
+      });
+    }
+  
+  }
+
+
+}
